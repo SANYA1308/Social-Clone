0467dab95101c5c6402c098e15d2842ce376a3b0
Sanya <sanyagondi1308@gmail.com>
2022-01-29 18:20:35 +0530

    Social-Clone

---
+++ simplesocial/accounts/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import models
+from django.contrib import auth
+# Create your models here.
+
+class User(auth.models.User,auth.models.PermissionsMixin):
+
+    def __str__(self):
+        return "@{}".format(self.username)
+
